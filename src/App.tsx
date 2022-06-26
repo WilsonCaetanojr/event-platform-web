4916304e7509cbf8d7c1a0f7a465121dac22daa8
Wilson Caetano <wilson@qyon.com>
2022-06-26 18:52:17 -0300

    Revert "Create components Header and Sidebar"
    This reverts commit 590b08f31f963bbf80a66e21a901ee54ef7794e1.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Event } from "./pages/Event";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
 
 function App() {
-  return (
-    <>
-      <Event />
-    </>
-  );
+  const { data } = useQuery(GET_LESSONS_QUERY);
+
+  return <h1>Init</h1>;
 }
 
 export default App;
--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import { Logo } from "../assets/Logo";
-
-export function Header() {
-  return (
-    <header className="w-full p-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
-      <Logo />
-    </header>
-  );
-}
