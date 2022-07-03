1b03393ba4d5e288c4f7ecae1a4718bfa91a0b59
Wilson Caetano <wilson@qyon.com>
2022-07-03 15:24:05 -0300

    Configure envs Apollo connection

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: process.env.VITE_APP_GRAPHQL_API_URL,
+  uri: `${import.meta.env.VITE_API_URL}/v2/cl4u0etmj0ebt01ue4y4n68fa/master`,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
